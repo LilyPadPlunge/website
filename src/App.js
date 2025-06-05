81432fa4ddb245d40a77f5deafe43fffb58e7c87
LilyPadPlunge <lilypadplunge@gmail.com>
2025-06-04 22:42:53 -0700

    made available on gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Im gonna poop <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
